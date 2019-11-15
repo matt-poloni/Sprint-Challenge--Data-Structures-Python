cd6ec5196b467151243a2733a358caa825954c3f
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-11-15 11:47:21 -0500

    Imports BST & dependencies from the week, refactors names w/ it

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -9,12 +10,17 @@ with open('names_2.txt', 'r') as f:
     names_2 = f.read().split("\n")  # List containing 10000 names
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Initialize BST w/ first name in names_1
+bst = BinarySearchTree(names_1[0])
+# Insert the rest of the names from names_1
+for name_1 in names_1[1:]:
+    bst.insert(name_1)
+# Search BST for each name in name_2
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
-end_time = time.time()
+end_time = time.time() # First run in 0.1 seconds
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
